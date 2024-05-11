c1c16c0e4eef78a1074d05a0456af56e6772dac3
ashikreserveit17 <ashik.bhuyan@reserveitbd.com>
2024-05-11 19:08:42 +0600

    first commit

---
+++ test.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex mt-[100px]">
+        <div className="w-1/4 h-screen fixed overflow-y-auto">
+          <div className="space-y-2">
+            {' '}
+            <p className="text-[48px] text-[#e2e8f0] font-bold">
+              Brittany Chiang
+            </p>
+            <p className="text-[20px] text-[#e2e8f0] font-semibold">
+              Senior Frontend Engineer
+            </p>
+            <p className="text-[16px] text-[#94a3b8] ">
+              I build pixel-perfect, engaging, and accessible digital
+              experiences.
+            </p>
+          </div>
+        </div>
+        <div className="w-3/4 h-screen  ml-[35%] ">
+          Main Content Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Ducimus omnis quasi velit obcaecati esse cumque magnam incidunt aut
+          eum corrupti cupiditate sed saepe pariatur ullam, hic fuga vero
+          tenetur iure sequi expedita quaerat laudantium doloribus. Quidem
+          possimus quo qui aut recusandae, tenetur incidunt placeat! Expedita
+          unde porro officiis, dolorum nam fuga deserunt dicta, amet molestiae,
+          ipsam quae quasi? Laboriosam, praesentium saepe! Sed voluptatibus
+          cupiditate explicabo a debitis, sunt distinctio minima reiciendis
+          corporis illum perferendis esse quam culpa repudiandae neque
+          repellendus molestiae maxime. Aliquid perspiciatis suscipit minus
+          Voluptate neque doloribus esse dicta itaque, placeat nemo vero
+          repellendus earum ipsam! Reiciendis recusandae quisquam quae magnam.
+          Nesciunt nemo odit facere perspiciatis nostrum doloremque magni vel
+          sint iste praesentium, libero esse aut quidem natus laborum veritatis
+          eaque neque itaque minus. Iure, aperiam voluptas, minima debitis
+          aliquid veniam, porro doloribus obcaecati perspiciatis magnam sed
+          nesciunt eligendi corporis quidem, vitae adipisci ex dignissimos
+          officiis unde rem eaque repellat. Veniam blanditiis quo ex
+          dignissimos, aspernatur earum distinctio beatae modi quod nemo saepe
+          libero facilis nobis? Ratione ducimus pariatur excepturi repellendus
+          deleniti! Minus sit dolore minima suscipit est quod eius mollitia
+          tempore, quia omnis tempora consequuntur cum enim numquam voluptatibus
+          maiores nulla odio ad commodi possimus ipsum perspiciatis adipisci?
+          Sunt delectus debitis accusamus voluptatibus ullam autem quas
+          dignissimos, natus libero iusto molestiae impedit, animi excepturi
+          saepe non, necessitatibus praesentium. Temporibus voluptatem itaque
+          ipsa, omnis nesciunt est sapiente illum soluta veritatis. Blanditiis
+          perferendis doloremque porro molestias omnis placeat rerum voluptate
+          voluptas, voluptatibus velit cum accusamus deleniti asperiores impedit
+          facilis nemo, amet consectetur sint saepe architecto voluptatum
+          distinctio id et. Molestias maiores, similique quo est molestiae iste
+          tempora eos itaque deleniti voluptatum, a rerum expedita ea earum.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
